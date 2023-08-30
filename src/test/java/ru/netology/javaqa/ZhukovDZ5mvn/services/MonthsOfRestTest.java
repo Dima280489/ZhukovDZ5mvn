c69173491b77a1a9a82cee21ce0fa38413f73f11
Neto test <demon_4@mail.ru>
2023-08-30 22:21:57 +0300

    Parameterized test

--- src/test/java/ru/netology/javaqa/ZhukovDZ5mvn/services/MonthsOfRestTest.java
+++ src/test/java/ru/netology/javaqa/ZhukovDZ5mvn/services/MonthsOfRestTest.java
@@ -7,14 +7,23 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class MonthsOfRestTest {
 
-    @Test
-    public void testMonthOfRest() {
+    //    @Test
+//    public void testMonthOfRest() {
+//        MonthsOfRest service = new MonthsOfRest();
+//
+//        int expected = 3;
+//        int actual = service.calcMonth(10_000, 3_000, 20_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+    @ParameterizedTest
+    @CsvSource({
+            "10000,3000,20000",
+            "100000,60000,150000"
+    })
+    public void testMonthOfRest(int income, int expenses, int threshold) {
         MonthsOfRest service = new MonthsOfRest();
-
-        int expected = 3;
-        int actual = service.calcMonth(10_000, 3_000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
+        int actual = service.calcMonth(income, expenses, threshold);
     }
 }
 
